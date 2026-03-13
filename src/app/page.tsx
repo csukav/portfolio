33a813d9c86d6a2fc7ccb83bd6b017b2db2074a5
Revivent <csukav@gmail.com>
2026-03-13 07:48:19 +0100

    Update portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,12 @@ import SkillsSection from "@/components/SkillsSection";
 import ContactSection from "@/components/ContactSection";
 import Footer from "@/components/Footer";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 
 export default function Home() {
   return (
     <main>
+      <Analytics />
       <SpeedInsights />
       <Navbar />
       <HeroSection />
